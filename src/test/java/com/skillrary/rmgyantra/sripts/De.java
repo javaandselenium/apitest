3169f79a9e068e66f9923cfa841c180f6fe29d9f
QSP <QSP@QSP>
2021-10-02 09:31:08 +0530

    Hello

---
+++ src/test/java/com/skillrary/rmgyantra/sripts/De.java
@@ -0,0 +1,13 @@
+package com.skillrary.rmgyantra.sripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class De {
+
+	@Test
+	public void tc4() {
+		Reporter.log("Hello APi");
+	}
+
+}
